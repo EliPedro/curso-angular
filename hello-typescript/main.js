41d14dc07df62786d8696f83a6eefb22188a1964
eli <elipedrodos.santos@gmail.com>
2019-08-29 23:00:51 -0300

    TypeScript

---
+++ hello-typescript/main.js
@@ -0,0 +1,20 @@
+var minhaVar = "minha variavel";
+function minhaFunc(x, y) {
+    return x + y;
+}
+var num = 2;
+var pi = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.Soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+// tsc main.js
